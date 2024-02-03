2e415152b0c5c0c94ea347ee07554348048e9a61
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-03 18:04:32 +0600

    task 1 is complete

--- task1.js
+++ task1.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+// task 01 - Take four parameters. Multiply the four numbers and then return the result
+function multiply(num1,num2,num3,num4){
+    const multiply = num1 * num2 * num3 * num4;
+    return multiply;
+}
+// call
+const numbers = multiply(2,2,2,2);
+console.log("the multiply is:",numbers);
+
+// the task is complete 
